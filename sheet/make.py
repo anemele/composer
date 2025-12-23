8c7ff54fe24bd67255bb56e6409bfea9d0fa0516
Jason Swift <anemele@outlook.com>
2025-12-23 20:09:38 +0800

    add a make script

---
+++ sheet/make.py
@@ -0,0 +1,39 @@
+"""自动生成音乐文件：wav 和 flac"""
+
+from pathlib import Path
+from subprocess import run
+
+this_path = Path(__file__).parent
+music_path = this_path / "music"
+if not music_path.exists():
+    music_path.mkdir()
+    music_path.joinpath(".gitignore").write_text("*")
+
+manifest = """
+4536251
+月半小夜曲 melody1 melody2
+御剑江湖
+浮光
+穿越时空的思念
+"""
+
+
+def make_wav_and_flac(*src: Path, dst: Path):
+    wav = dst.with_suffix(".wav")
+    if not wav.exists():
+        cmd = ["composer", *src, "--save", wav]
+        run(cmd)
+    flac = dst.with_suffix(".flac")
+    if not flac.exists():
+        cmd = ["ffmpeg", "-i", wav, flac]
+        run(cmd)
+
+
+for line in manifest.strip().splitlines():
+    name, *items = line.strip().split()
+    mf = music_path / name
+
+    if items:
+        make_wav_and_flac(*(this_path / item for item in items), dst=mf)
+    else:
+        make_wav_and_flac(this_path / name, dst=mf)
